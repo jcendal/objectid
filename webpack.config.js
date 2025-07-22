f22a53d797cf9970b8a017355003c6afc36586df
jcendal <jcendal@rebase.es>
2025-07-22 18:47:23 +0200

    chore: rename objectid-lite to objectid

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, './lib'),
     filename: 'index.js',
-    library: '@jcendal/objectid-lite',
+    library: '@jcendal/objectid',
     libraryTarget: 'umd',
     umdNamedDefine: true,
     libraryExport: 'default'
